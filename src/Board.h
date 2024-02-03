f640959959e7deff5a86aa2daea6ebffca8d2292
eDi9root <119016575+eDi9root@users.noreply.github.com>
2024-02-03 00:03:47 -0500

    EDIT: board.h

--- src/Board.h
+++ src/Board.h
@@ -2,8 +2,8 @@
 #define BOARD_H
 
 
-class Board
-{
-    Private:
-        chessPiece* board[8][8];
-}
+class Board: public subject {
+  public:
+    static const int ROWS = 8;
+    static const int COLS = 8;
+};
