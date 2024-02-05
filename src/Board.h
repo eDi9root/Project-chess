8ae25e6739a4e9a61bf980d8ee453c6a02222811
eDi9root <119016575+eDi9root@users.noreply.github.com>
2024-02-04 22:36:25 -0500

    EDIT: board.h

--- src/Board.h
+++ src/Board.h
@@ -1,9 +1,24 @@
 #ifndef BOARD_H
 #define BOARD_H
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
 
 
 class Board: public subject {
   public:
     static const int ROWS = 8;
     static const int COLS = 8;
+  private:
+    vector<vector<Square>> board;
+    vector<move> moved;
+  public:
+    Board();
+    ~Board();
+    void init();
+    
 };
+
+#endif
